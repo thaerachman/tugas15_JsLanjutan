0ff5edbca5b8054ef734f53770e09527e70218e7
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-25 23:26:08 +0700

    Add files via upload

---
+++ tugas_15.js
@@ -0,0 +1,10 @@
+var ab = 8
+var bc = 6
+
+var a = Math.pow(ab,2)
+var b = Math.pow(bc,2)
+var ac = Math.sqrt(a+b)
+
+console.log("nilai ab : ",ab)	
+console.log("nilai bc : ",bc)
+console.log("panjang sisi ac pada segitiga siku-siku tersebut : ",ac," cm")
